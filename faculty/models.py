924f4af8ec925d22fe359bbcee99e200b033521a
Anshu Musaddi <anshumusaddi@gmail.com>
2018-12-19 00:35:29 +0530

    added faculty model

--- faculty/models.py
+++ faculty/models.py
@@ -1,4 +1,10 @@
 from django.db import models
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class FacultyProfileInfo(models.Model):
+    fid = models.CharField(max_length=100,primary_key=True)
+    user = models.OneToOneField(User,related_name='faculty',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.username
--- officeadmin/models.py
+++ officeadmin/models.py
@@ -2,10 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UserProfileInfo(models.Model):
+class AdminOfficeProfileInfo(models.Model):
 
    user = models.OneToOneField(User,related_name='adminuser',on_delete=models.CASCADE)
-
-   fId = models.CharField(max_length=100)
    def __str__(self):
        return self.user.username
